36823b42761af6e5dbec10ed6d574aeddb408f55
Samuel Diggins <sam@sjd.is>
2020-06-03 20:18:09 +0100

    initial commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Art Gift</h1>
+  </Layout>
+)
+
+export default IndexPage
