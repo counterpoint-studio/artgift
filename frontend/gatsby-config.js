419fd92296266fdeb446c15345729f276629a2b9
Tero Parviainen <tero@teropa.info>
2020-06-04 19:15:37 +0300

    Basic Firebase Firestore setup

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -26,6 +30,20 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        },
       },
     },
     {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import React from "react"
 import { useIntl } from "gatsby-plugin-intl"
+import firebase from "gatsby-plugin-firebase"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
   let intl = useIntl()
+
+  // Just for testing Firebase connection
+  React.useEffect(() => {
+    firebase
+      .firestore()
+      .collection("/timeslots")
+      .get()
+      .then(snapshot => {
+        console.log(snapshot)
+      })
+  }, [])
+
   return (
     <Layout>
       <SEO title="Home" />
