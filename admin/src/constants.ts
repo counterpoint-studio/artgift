b520f828040d7118e14bf1e9884899cef9b693e5
Tero Parviainen <tero@teropa.info>
2020-06-11 13:27:30 +0300

    basic slot admin

---
+++ admin/src/constants.ts
@@ -0,0 +1,4 @@
+export const REGIONS = ['ETELÄINEN', 'LÄNTINEN', 'KESKINEN', 'KOILLINEN', 'ITÄINEN & ÖSTERSUNDOM'];
+export const DATES = ['20200807', '20200808', '20200809', '20200814', '20200815', '20200816'];
+export const HOURS = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+export const MINUTES = [0, 30];
--- frontend/src/services/gifts.ts
+++ frontend/src/services/gifts.ts
@@ -1,40 +1,22 @@
+import firebase from 'gatsby-plugin-firebase';
+
 export type GiftSlot = {
     id: string;
     region: string;
-    time: number;
+    date: string;
+    time: string;
     status: 'available' | 'reserved' | 'onHold'
 }
 
 export function subscribeToGiftSlotsOverview(callback: (slots: GiftSlot[]) => void) {
-    // Mock response
-    setTimeout(() => {
-        let data: GiftSlot[] = [];
-        let id = 0;
-        for (let day of [7, 8, 9]) {
-            for (let hour of [12, 13, 14, 15, 16, 17, 18, 19]) {
-                for (let region of ['ETELÄINEN', 'LÄNTINEN', 'KESKINEN', 'KOILLINEN', 'ITÄINEN & ÖSTERSUNDOM']) {
-                    data.push({
-                        id: `${id++}`,
-                        region,
-                        status: 'available',
-                        time: new Date(2020, 7, hour, 0).getTime()
-                    }, {
-                        id: `${id++}`,
-                        region,
-                        status: 'available',
-                        time: new Date(2020, 7, day, hour, 30).getTime()
-                    })
-                }
-                for (let region of ['POHJOINEN', 'KAAKKOINEN']) {
-                    data.push({
-                        id: `${id++}`,
-                        region,
-                        status: 'available',
-                        time: new Date(2020, 7, day, hour, 0).getTime()
-                    })
-                }
-            }
-        }
-        callback(data)
-    }, 100);
+    let unSub = firebase.firestore().collection("slots").orderBy("date")
+        .orderBy("time")
+        .orderBy("region")
+        .onSnapshot((slotsSnapshot) => {
+            callback(
+                slotsSnapshot.docs.map((d) => ({ id: d.id, ...d.data() } as GiftSlot))
+            );
+        });
+    return unSub
+
 }
