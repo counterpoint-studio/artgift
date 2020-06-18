a2240990f88deee977300e52fda5c1ef8ecb0e2f
Tero Parviainen <tero@teropa.info>
2020-06-18 12:51:53 +0300

    admin view refinement for slots & gifts

---
+++ admin/src/types.ts
@@ -0,0 +1,26 @@
+
+export type Slot = {
+    id?: string;
+    date: string;
+    time: string;
+    region: string;
+    status: "available" | "reserved" | "onHold";
+};
+
+export type Gift = {
+    id: string;
+    slotId: string;
+    toName: string;
+    toAddress: string;
+    toLocation?: GiftLocation;
+    toLanguage: string;
+    toSignificance: string;
+    fromName: string;
+    fromPhoneNumber: string;
+    fromEmail: string;
+    fromMessage: string;
+};
+export type GiftLocation = {
+    region: string;
+    point: [number, number];
+};
