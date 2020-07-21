c8108fff8f0493962dbdf0e5904177fb4c2b950e
Tero Parviainen <tero@teropa.info>
2020-07-21 10:36:19 +0300

    remove the dates for the second weekend from the admin

--- admin/src/constants.ts
+++ admin/src/constants.ts
@@ -5,7 +5,7 @@ export const REGIONS = [
     { id: 'KOILLINEN', name: { fi: 'Koillinen', en: 'Northeastern' } },
     { id: 'ITÄINEN & ÖSTERSUNDOM', name: { fi: 'Itäinen & Östersundom', en: 'Eastern & Östersundom' } }
 ];
-export const DATES = ['20200807', '20200808', '20200809', '20200814', '20200815', '20200816'];
+export const DATES = ['20200807', '20200808', '20200809'];
 export const HOURS = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 export const MINUTES = [0, 30];
 
