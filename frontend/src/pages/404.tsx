98f1b3047453b2b91b1a4e2fb5aa0b048a43afc4
Samuel Diggins <sam@sjd.is>
2020-07-23 10:16:45 +0100

    custom 404 page + fixing masking grid overlap issue

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,16 +1,32 @@
 import React from "react"
+import Helmet from "react-helmet"
 import { useIntl } from "gatsby-plugin-intl"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import NextButton from "../components/nextButton"
 
 const NotFoundPage = () => {
   let intl = useIntl()
   return (
     <Layout>
-      <SEO title={intl.formatMessage({ id: "404SEOTitle" })} />
-      <h1>{intl.formatMessage({ id: "404Title" })}</h1>
-      <p>{intl.formatMessage({ id: "404Body" })}</p>
+      <SEO
+        title={intl.formatMessage({ id: "404SEOTitle" })}
+        description={intl.formatMessage({ id: "404SEODescription" })}
+      />
+      <Helmet
+        bodyAttributes={{
+          class: "404",
+        }}
+        key="helmet"
+      />
+      <main className="main">
+        <div className="scroll">
+          <h1>{intl.formatMessage({ id: "404Title" })}</h1>
+          <p>{intl.formatMessage({ id: "404Body" })}</p>
+          <NextButton to="/" text={intl.formatMessage({ id: "homeButton" })} />
+        </div>
+      </main>
     </Layout>
   )
 }
